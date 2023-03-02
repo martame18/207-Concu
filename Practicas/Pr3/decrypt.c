c096386c187226962575b18f72902c7b47d67eb7
martame18 <martamaleno.185@hotmail.com>
2023-03-02 14:13:37 +0100

    desarrollo de la práctica, descifrado de ficheros

--- Practicas/Pr3/decrypt.c
+++ Practicas/Pr3/decrypt.c
@@ -8,19 +8,23 @@
  * 	Podemos usar la notaci�n de array con v y k
  * 	v[0] v[1] --- k[0] ... k[3]
  */
-void decrypt(unsigned int *v, unsigned int *k)
-{
+void decrypt(unsigned int *v, unsigned int *k){
 	//Definir variables e inicializar los valores de delta y sum
+    unsigned int delta = 0x9e3779b9;
+    unsigned int sum = 0xC6EF3720;
+    int i;
 
 	//Repetir 32  veces (usar un bucle) la siguiente secuencia de operaciones de bajo nivel
-
+    for (i=0; i<32; i++){
 			//Restar a v[1] el resultado de la operacion :
 				// (v[0] desplazado a la izquierda 4 bits +k[2]) XOR (v[0] + sum)  XOR (v[0] desplazado a la derecha 5 bits)+k[3]
-
+            v[1] -= ((v[0]<<4) + k[2]) ^ (v[0] + sum) ^ ((v[0]>>5) + k[3]);
 			//Restar a v[0] el resultado de la operacion:
 				// (v[1] desplazado a la izquierda 4 bits + k[0]) XOR (v[1]+ sum)  XOR (v[1] desplazado a la derecha 5 bits)+k[1]
-
+            v[0] -= ((v[1]<<4) + k[0]) ^ (v[1]+sum) ^ ((v[1]>>5) + k[1]);
 			// Restar a sum el valor de delta
+            sum -= delta;   
+    }
 }
 
 /* Parte 2: Metodo main. Tenemos diferentes opciones para obtener el nombre del fichero cifrado y el descifrado
@@ -28,36 +32,71 @@ void decrypt(unsigned int *v, unsigned int *k)
  * 2. Pedir que el usuario introduzca los nombres por teclado
  * 3. Definir arrays de caracteres con los nombres
  */
-int main(int argc, char *argv[] )
-{
+int main(int argc, char *argv[] ){
 	/*Declaraci�n de las variables necesarias, por ejemplo:
 	* variables para los descriptores de los ficheros ( FILE * fCif, *fDes)
 	* la constante k inicializada con los valores de la clave
 	* buffer para almacenar los datos (puntero a unsigned int, m�s adelante se reserva memoria din�mica */
+    FILE *fCif, *fDes;
+    unsigned int *buffer;
+    unsigned int k[4] = {128, 129, 130, 131};
+    int imgSize;  //tamaño imagen descifrada
+    int offset;   //(imgSize + offset)%8 == 0
 
+    if (argc < 3){
+        perror("Error  -  Faltan argumentos de entrada");
+        exit(EXIT_FAILURE);
+    }
 	/*Abrir fichero encriptado fCif en modo lectura binario
 	 * nota: comprobar que se ha abierto correctamente*/
-    
+    fCif = fopen(argv[1], "rb");  // rb = read binary
+    if(fCif == NULL){
+        perror("Error  -  Fichero cifrado no se puede abrir");
+        exit(EXIT_FAILURE);
+    }
 
-	/*Abrir/crear fichero fDes en modo escritura binario
+	/*Abrir/crear fichero fDes en modo escritura binaria
 	 * nota: comprobar que se ha abierto correctamente*/
-    
+    fDes = fopen(argv[2], "wb");  // wb = write binary
+    if (fDes == NULL){
+        fclose(fCif);
+        perror("Error  -  Fichero descifrado no se puede abrir");
+        exit(EXIT_FAILURE);
+    }
 
    /*Al comienzo del fichero cifrado esta almacenado el tama�o en bytes que tendr� el fichero descifrado.
     * Leer este valor (imgSize)*/
-
+    fread(&imgSize, sizeof(int), 1, fCif); //fread se usa en ficheros binarios, fscanf en ficheros de texto
+    offset = (imgSize%8 == 0) ?  0 : (8-(imgSize%8)); //si la condición entre paréntesis es true offset toma el valor antes de : y si es false, el de después
 	/*Reservar memoria din�mica para el buffer que almacenara el contenido del fichero cifrado
 	 * nota1: si el tama�o del fichero descifrado (imgSize) no es m�ltiplo de 8 bytes,
 	 * el fichero cifrado tiene adem�s un bloque de 8 bytes incompleto, por lo que puede que no coincida con imgSize
 	 * nota2: al reservar memoria din�mica comprobar que se ha realiz� de forma correcta */
+    buffer = (unsigned int *)malloc(imgSize + offset);
+    if(buffer == NULL){
+        fclose(fCif);
+        fclose(fDes);
+        perror("Error  -  No se ha podido reservar memoria en el buffer");
+        exit(EXIT_FAILURE);
+    }
 
 	/*Leer la informaci�n del fichero cifrado, almacenado el contenido en el buffer*/
+    int leidos = fread(buffer, sizeof(unsigned int), (imgSize + offset), fCif);
+    printf("Se pide leer %d y se leen %d\n",(imgSize + offset), leidos);
 
 	/*Para cada bloque de 64 bits (8 bytes o dos unsiged int) del buffer, ejectuar el algoritmo de descifrado*/
+    int i;
+    for(i=0; i<leidos; i+=2){
+        decrypt(&buffer[i], k);
+    }
 
     /*Guardar el contenido del buffer en el fichero fDes
      * nota: en fDes solo se almacenan tantos bytes como diga imgSize */
+    fwrite(buffer, sizeof(unsigned int), imgSize, fDes);
 
 	/*Cerrar los ficheros*/
+    fclose(fCif);
+    fclose(fDes);
+    free(buffer);
 }
 
